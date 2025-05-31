856370aabb0b11f2ef29d1f8f86d45b53fb8ab5b
Vedank Purohit <VedankPurohit2@gmail.com>
2025-05-31 12:08:18 +0530

    Time: 366 ms (45.53%) | Memory: 60.9 MB (28.57%) - LeetSync

--- Leetcode/1962-single-threaded-cpu/single-threaded-cpu.py
+++ Leetcode/1962-single-threaded-cpu/single-threaded-cpu.py
@@ -1,31 +1,20 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        n = len(tasks)
-        indices = list(range(n))
-        indices.sort(key=lambda i: (tasks[i][0], i))
-        
-        class Task:
-            def __init__(self, idx):
-                self.idx = idx
-                
-            def __lt__(self, other):
-                if tasks[self.idx][1] != tasks[other.idx][1]:
-                    return tasks[self.idx][1] < tasks[other.idx][1]
-                return self.idx < other.idx
-        
-        minHeap = []
-        res = []
-        time = i = 0
-        while minHeap or i < n:
-            while i < n and tasks[indices[i]][0] <= time:
-                heapq.heappush(minHeap, Task(indices[i]))
+        for i, t in enumerate(tasks):
+            t.append(i)
+        tasks.sort(key=lambda t: t[0])
+
+        res, minHeap = [], []
+        i, time = 0, tasks[0][0]
+
+        while minHeap or i < len(tasks):
+            while i < len(tasks) and time >= tasks[i][0]:
+                heapq.heappush(minHeap, [tasks[i][1], tasks[i][2]])
                 i += 1
-                
             if not minHeap:
-                time = tasks[indices[i]][0]
+                time = tasks[i][0]
             else:
-                next_task = heapq.heappop(minHeap)
-                time += tasks[next_task.idx][1]
-                res.append(next_task.idx)
-                
+                procTime, index = heapq.heappop(minHeap)
+                time += procTime
+                res.append(index)
         return res
